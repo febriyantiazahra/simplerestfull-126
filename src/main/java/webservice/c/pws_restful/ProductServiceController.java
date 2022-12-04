86ff61d44f4b310061c2c8eb8529b5ba41ada154
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-12-04 22:04:57 +0700

    menambahkan DELETE API

--- src/main/java/webservice/c/pws_restful/ProductServiceController.java
+++ src/main/java/webservice/c/pws_restful/ProductServiceController.java
@@ -4,13 +4,22 @@
  * and open the template in the editor.
  */
 package webservice.c.pws_restful;
+
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Febriyanti Azahra
  */
+@RestController
 public class ProductServiceController {
     private static Map<String, Product> productRepo = new HashMap<>();
     static {
@@ -23,5 +32,12 @@ public class ProductServiceController {
         almond.setId("2");
         almond.setName("Almond");
         productRepo.put(almond.getId(), almond);
+    }
+
+    //DELETE API
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") String id) {
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
     }
 }
