e69c46d44470a636499c246ff2e70b64e7fdcf3f
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-12-04 20:31:32 +0700

    membuat class controller ProductService

--- src/main/java/webservice/c/pws_restful/ProductServiceController.java
+++ src/main/java/webservice/c/pws_restful/ProductServiceController.java
@@ -5,10 +5,24 @@
  */
 package webservice.c.pws_restful;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Febriyanti Azahra
  */
 public class ProductServiceController {
-    
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product honey = new Product();
+        honey.SetId("1");
+        honey.SetName("Honey");
+        productRepo.put(honey.getId(), honey);
+        
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
 }
