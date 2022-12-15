9c2ab3eea8ba272319c5f0486816f96ec150b25a
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-12-15 09:44:12 +0700

    menambahkan quantity & price

--- src/main/java/webservice/c/pws_restful/Product.java
+++ src/main/java/webservice/c/pws_restful/Product.java
@@ -12,6 +12,8 @@ package webservice.c.pws_restful;
 class Product {
     private String id;
     private String name;
+    private String qty;
+    private String price;
 
     public String getId() {
         return id;
@@ -27,5 +29,21 @@ class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 }
--- src/main/java/webservice/c/pws_restful/ProductServiceController.java
+++ src/main/java/webservice/c/pws_restful/ProductServiceController.java
@@ -26,11 +26,15 @@ public class ProductServiceController {
         Product honey = new Product();
         honey.setId("1");
         honey.setName("Honey");
+        honey.setQty("2");
+        honey.setPrice("25.000");
         productRepo.put(honey.getId(), honey);
         
         Product almond = new Product();
         almond.setId("2");
         almond.setName("Almond");
+        almond.setQty("1");
+        almond.setPrice("15.000");
         productRepo.put(almond.getId(), almond);
     }
 
